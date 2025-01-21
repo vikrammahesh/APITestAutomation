3e93bcadae1129a616570c7b23d44654f4aca6c6
Mahesh_Vikram <mahesh_vikram@epam.com>
2025-01-21 17:42:03 +0530

    Revert "Added files"
    This reverts commit 28434df99ff09867cc25d6defa4eddfe4ef328c8.

--- src/test/java/com/api/tests/APITests.java
+++ src/test/java/com/api/tests/APITests.java
@@ -1,6 +1,7 @@
 package com.api.tests;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 public class APITests {
