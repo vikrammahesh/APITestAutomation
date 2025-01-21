56fcd1e2a7e73624262eb5fad8860991b9f03abe
Mahesh_Vikram <mahesh_vikram@epam.com>
2025-01-21 17:46:00 +0530

    Added files

--- src/test/java/com/api/tests/APITests.java
+++ src/test/java/com/api/tests/APITests.java
@@ -1,7 +1,6 @@
 package com.api.tests;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.*;
-import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
 public class APITests {
