c791812768a12a4afb407786647087ff9332b5ae
Mahesh_Vikram <mahesh_vikram@epam.com>
2025-01-21 17:37:28 +0530

    Added files

---
+++ src/test/java/com/api/tests/APITests.java
@@ -0,0 +1,24 @@
+package com.api.tests;
+import org.testng.annotations.Test;
+import static io.restassured.RestAssured.*;
+import static io.restassured.matcher.RestAssuredMatchers.*;
+import static org.hamcrest.Matchers.*;
+
+public class APITests {
+
+    @Test
+    public void login() {
+        given()
+                .baseUri("http://64.227.160.186:8080/api/auth/")
+                .header("Content-type","application/json")
+                .body("{\"username\": \"uday1234\", \"password\": \"uday12345\"}")
+                .when()
+                .log().all()
+                .post("/login")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .time(lessThan(3000L));
+    }
+
+}
